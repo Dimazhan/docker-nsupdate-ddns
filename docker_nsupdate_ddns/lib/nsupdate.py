8454a410ae529136e342093a0763339f02b2f7d6
Dimazhan <dimazhan@list.ru>
2025-05-15 20:55:12 +0700

    remove host records before updating

--- docker_nsupdate_ddns/lib/nsupdate.py
+++ docker_nsupdate_ddns/lib/nsupdate.py
@@ -15,6 +15,7 @@ def add_records(records):
     keyring = dns.tsigkeyring.from_text({config['TSIG_NAME']: config['TSIG_KEY']})
 
     for hostname, ip in records.items():
+        delete_records({hostname:None})
         LOG.info(f"Adding record for {hostname}({ip['IPv4'], ip['IPv6']})")
 
         for proto, addr in ip.items():
@@ -44,24 +45,28 @@ def delete_records(records):
     keyring = dns.tsigkeyring.from_text({config['TSIG_NAME']: config['TSIG_KEY']})
 
     for hostname, ip in records.items():
-        LOG.info(f"Deleting record for {hostname}({ip['IPv4'], ip['IPv6']})")
+        if ip == None:
+            LOG.info(f"Deleting record for {hostname}")
+        else:
+            LOG.info(f"Deleting record for {hostname}({ip['IPv4'], ip['IPv6']})")
 
-        for proto, addr in ip.items():
-            update = dns.update.Update(config['DOMAIN'], keyring=keyring)
-            update.delete(hostname)
-            dns.query.tcp(update, config['NAMESERVER'], timeout=2, port=int(config['PORT']))
+        update = dns.update.Update(config['DOMAIN'], keyring=keyring)
+        update.delete(hostname)
+        dns.query.tcp(update, config['NAMESERVER'], timeout=2, port=int(config['PORT']))
 
-            if proto == 'IPv4' and 'REVERSE4_DOMAIN' in config:
-                reventry = dns.reversename.from_address(addr)
-                update = dns.update.Update(config['REVERSE4_DOMAIN'], keyring=keyring)
-                update.delete(reventry)
-                dns.query.tcp(update, config['NAMESERVER'], timeout=2, port=int(config['PORT']))
+        if ip != None:
+            for proto, addr in ip.items():
+                if proto == 'IPv4' and 'REVERSE4_DOMAIN' in config:
+                    reventry = dns.reversename.from_address(addr)
+                    update = dns.update.Update(config['REVERSE4_DOMAIN'], keyring=keyring)
+                    update.delete(reventry)
+                    dns.query.tcp(update, config['NAMESERVER'], timeout=2, port=int(config['PORT']))
 
-            if proto == 'IPv6' and 'REVERSE6_DOMAIN' in config:
-                reventry = dns.reversename.from_address(addr)
-                update = dns.update.Update(config['REVERSE6_DOMAIN'], keyring=keyring)
-                update.delete(reventry)
-                dns.query.tcp(update, config['NAMESERVER'], timeout=2, port=int(config['PORT']))
+                if proto == 'IPv6' and 'REVERSE6_DOMAIN' in config:
+                    reventry = dns.reversename.from_address(addr)
+                    update = dns.update.Update(config['REVERSE6_DOMAIN'], keyring=keyring)
+                    update.delete(reventry)
+                    dns.query.tcp(update, config['NAMESERVER'], timeout=2, port=int(config['PORT']))
 
 
 def init(_config):
